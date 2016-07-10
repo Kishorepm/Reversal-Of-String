bd2c615c0e695a85669aed2148dd0492557b329b
Kishorepm <kishorepmofficial@gmail.com>
2016-07-10 10:31:11 +0530

    Update towerof human.c

--- towerof human.c
+++ towerof human.c
@@ -4,17 +4,20 @@ int main()
 {
     char source='A',temp='B',des='C';
     int disk;
-    printf("");
+    printf("Enter number of disks");
     scanf("%d",&disk);
-    toh(disk,sou,tem,disk);
+    toh(disk,sou,temp,disk);
     return 0;
     }
-void toh(int disk,char source,char temp,char dest)
+void toh(int disk,char source,char temp,char des)
 {
     {
         if(disk==1)
+        {
+            printf("From %c to %c\n",disk,sou,des);
+        }
      }
 toh(disk-1,sou,des,temp);
-pf("",disk,source,des);
+printf("From %c to %c\n",disk,sou,des);
 toh(disk-1,temp,sou,des);
 }
