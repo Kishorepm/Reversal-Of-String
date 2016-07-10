2b75f6f0c3a8222339a08bf4df84f61531055843
Kishorepm <kishorepmofficial@gmail.com>
2016-07-10 11:32:27 +0530

    Update towerof human.c

--- towerof human.c
+++ towerof human.c
@@ -2,22 +2,22 @@
 void toh(int disk,char sou,char temp,char des);
 int main()
 {
-    char source='A',temp='B',des='C';
+    char sou='A',temp='B',des='C';
     int disk;
     printf("Enter number of disks");
     scanf("%d",&disk);
-    toh(disk,sou,temp,disk);
+    thunai(disk,sou,temp,disk);
     return 0;
     }
-void toh(int disk,char source,char temp,char des)
+void thunai(int disk,char source,char temp,char des)
 {
     {
         if(disk==1)
         {
-            printf("From %c to %c\n",disk,sou,des);
+            printf("From %c to %c\n",sou,des);
         }
      }
-toh(disk-1,sou,des,temp);
-printf("From %c to %c\n",disk,sou,des);
-toh(disk-1,temp,sou,des);
+thunai(disk-1,sou,des,temp);
+printf("From %c to %c\n",sou,des);
+thunai(disk-1,temp,sou,des);
 }
